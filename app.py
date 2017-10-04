12c204a9f5c0562d9ad3531594301c3d1d4e9175
Valery Chen <cpan3@stuy.edu>
2017-10-04 18:04:06 -0400

    heading and some comments

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-#
+#Team Pass the Word
+#Holden Higgins and Carol Pan
 #SoftDev1 pd7
-#
-#
+#HW07- Do I Know You?
+#2017-10-05
 
 from flask import Flask, render_template, request, session, redirect
 import os #for secret key
@@ -11,21 +12,27 @@ app = Flask(__name__)
 #hard coded username and password combo :(
 user = "JohnSmith"
 pwd = "abc123"
+
+#we need a secret key
 app.secret_key = os.urandom(32)
 
 #assign following fxn to run when
 #root route requested
 @app.route("/")
 def hello_world():
+    print session
+    #this only activates if a session is active
     if 'username' in session:
         return "welcome john"
     else:
         return render_template("login.html")
 
+#page to process form submission    
 @app.route("/auth")
 def authenticate():
-    #print request.method
-    #print request.args
+    #diag:
+    print request.method
+    print request.args
     if request.args['username'] == user:
         if request.args['password'] == pwd:
             session['username'] = request.args['username']
@@ -35,6 +42,7 @@ def authenticate():
     else:
         return "Error: wrong username"
 
+#page to logout, will be accessible through link    
 @app.route("/logout")
 def end_session():
     session.pop('username')
